f8d3b6bcc585c1b148b55285beec3669bc996f79
Bobby Rathore <bobby@shipmnts.com>
2019-09-12 10:43:31 -0400

    [MODIFY] skeleton code to fit my python interpreter requirements. [ADD] .idea/ to .gitignore.

--- hide.py
+++ hide.py
@@ -12,50 +12,69 @@
 
 import sys
 
+
 # Parse the map from a given filename
 def parse_map(filename):
-	with open(filename, "r") as f:
-		return [[char for char in line] for line in f.read().split("\n")]
+    with open(filename, "r") as f:
+        return [[char for char in line] for line in f.read().split("\n")]
+
 
 # Count total # of friends on board
 def count_friends(board):
-    return sum([ row.count('F') for row in board ] )
+    return sum([row.count("F") for row in board])
+
 
 # Return a string with the board rendered in a human-friendly format
 def printable_board(board):
-    return "\n".join([ "".join(row) for row in board])
+    return "\n".join(["".join(row) for row in board])
+
 
 # Add a friend to the board at the given position, and return a new board (doesn't change original)
 def add_friend(board, row, col):
-    return board[0:row] + [board[row][0:col] + ['F',] + board[row][col+1:]] + board[row+1:]
+    return (
+        board[0:row]
+        + [board[row][0:col] + ["F"] + board[row][col + 1 :]]
+        + board[row + 1 :]
+    )
+
 
 # Get list of successors of given board state
 def successors(board):
-    return [ add_friend(board, r, c) for r in range(0, len(board)) for c in range(0,len(board[0])) if board[r][c] == '.' ]
+    return [
+        add_friend(board, r, c)
+        for r in range(0, len(board))
+        for c in range(0, len(board[0]))
+        if board[r][c] == "."
+    ]
+
 
 # check if board is a goal state
 def is_goal(board):
-    return count_friends(board) == K 
+    return count_friends(board) == K
+
 
 # Solve n-rooks!
 def solve(initial_board):
     fringe = [initial_board]
     while len(fringe) > 0:
-        for s in successors( fringe.pop() ):
+        for s in successors(fringe.pop()):
             if is_goal(s):
-                return(s)
+                return s
             fringe.append(s)
     return False
 
+
 # Main Function
 if __name__ == "__main__":
-    IUB_map=parse_map(sys.argv[1])
+    IUB_map = parse_map(sys.argv[1])
 
     # This is K, the number of friends
     K = int(sys.argv[2])
-    print ("Starting from initial board:\n" + printable_board(IUB_map) + "\n\nLooking for solution...\n")
+    print(
+        "Starting from initial board:\n"
+        + printable_board(IUB_map)
+        + "\n\nLooking for solution...\n"
+    )
     solution = solve(IUB_map)
-    print ("Here's what we found:")
-    print (printable_board(solution) if solution else "None")
-
-
+    print("Here's what we found:")
+    print(printable_board(solution) if solution else "None")

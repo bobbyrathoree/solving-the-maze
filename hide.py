8f71b32f93228428aac2cb6d875d9610472bcbb7
Bobby Rathore <bobby@shipmnts.com>
2019-09-15 18:52:30 -0400

    [ADD] final terminating condition for part 2.

--- hide.py
+++ hide.py
@@ -8,9 +8,16 @@
 #
 # The problem to be solved is this:
 # Given a campus map, find a placement of F friends so that no two can find one another.
-
 import sys
 from collections import deque
+import threading
+
+solution_board = list()
+check_started = False
+solution_found = False
+old_number_of_friends = 0
+ctr = 0
+time_factor = 1.0
 
 
 # Parse the map from a given filename
@@ -26,11 +33,15 @@ def printable_board(board):
 
 # Add a friend to the board at the given position, and return a new board (doesn't change original)
 def add_friend(board, row, col):
+    global check_started
     board_with_new_friend = (
         board[0:row]
         + [board[row][0:col] + ["F"] + board[row][col + 1 :]]
         + board[row + 1 :]
     )
+    if not check_started:
+        check_number_of_friends(board_with_new_friend)
+        check_started = True
     return board_with_new_friend
 
 
@@ -68,20 +79,30 @@ def successors(board):
     ]
 
 
-# ktof_friends = dict()
+def check_number_of_friends(board):
+    global ctr, time_factor, old_number_of_friends
+
+    if solution_found:
+        raise SystemExit
+
+    new_number_of_friends = sum([row.count("F") for row in board])
+    if old_number_of_friends == new_number_of_friends:
+        ctr += 1
+        time_factor *= 2
+    else:
+        ctr = 0
+        time_factor = 1.0
+        old_number_of_friends = new_number_of_friends
+    if ctr >= 5:
+        print("No solution found!\nPress Ctrl+C to exit the program.")
+        raise SystemExit
+
+    threading.Timer(time_factor, check_number_of_friends, [solution_board]).start()
 
 
 # check if board is a goal state
 def is_goal(board):
-    number_of_friends = sum([row.count("F") for row in board])
-    # ktof_friends.update(
-    #     {
-    #         str(number_of_friends): 1
-    #         if str(number_of_friends) not in ktof_friends
-    #         else ktof_friends[str(number_of_friends)] + 1
-    #     }
-    # )
-    return number_of_friends == K
+    return sum([row.count("F") for row in board]) == K
 
 
 def stringify(arr):
@@ -90,36 +111,41 @@ def stringify(arr):
 
 # Solve n-rooks!
 def solve(initial_board):
+    global solution_board
     fringe = deque()
     fringe.append(initial_board)
     while len(fringe) > 0:
         for s in successors(fringe.pop()):
             if is_goal(s):
                 return s
+            solution_board = s
             fringe.append(s)
-            # print(ktof_friends, '\n')
     return False
+
+
+def solution_found_checker():
+    if solution_found:
+        raise SystemExit
+    else:
+        threading.Timer(1.0, solution_found_checker).start()
 
 
 # Main Function
 if __name__ == "__main__":
     iub_map = parse_map(sys.argv[1])
-    # iub_map = [
-    #     [".", ".", ".", ".", "&", "&", "&"],
-    #     [".", "&", "&", "&", ".", ".", "."],
-    #     [".", ".", ".", ".", "&", ".", "."],
-    #     [".", "&", ".", "&", ".", ".", "."],
-    #     [".", "&", ".", "&", ".", "&", "."],
-    #     ["#", "&", ".", ".", ".", "&", "@"],
-    # ]
 
     # This is K, the number of friends
     K = int(sys.argv[2])
+
     print(
         "Starting from initial board:\n"
         + printable_board(iub_map)
         + "\n\nLooking for solution...\n"
     )
+
     solution = solve(iub_map)
     print("Here's what we found:")
-    print(printable_board(solution) if solution else "None")
+    solution_found_checker()
+    print(printable_board(solution))
+    solution_found = True
+    raise SystemExit
